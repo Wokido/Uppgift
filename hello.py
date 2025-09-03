5862722d5003b078b5a0e7df59ab8714f5a73b94
Wokido <nilslukasmalcon.garpo@elev.ga.dbgy.se>
2025-09-03 13:06:19 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,34 @@
+print("10+67=",10+67)#plus
+print("67-14=",67-14)#minus
+print("5*8=",5*8)#multiply
+print("56/7=",56/7)#divide
+print("10//3=",10//3)#floor division Dividerar och rundar ner till närmaste heltal.
+print("10%3=",10%3)#modulus Tar resten vid heltalsdivision
+print("2**3=",2**3)#exponentiation Upphöjt till
+
+#uppgift 2
+
+print("Hej! Välkommen till mitt program.") 
+namn =input("vad heter du?")
+print(f"Trevligt att träffas, {namn}! Jag heter Lukas.")
+
+age =input("how old are you")
+print(f"WOW du är {age} ig")
+
+#Uppgift 3
+
+x = float(input ("Ange första talet"))
+y = float(input ("Ange andra talet"))
+
+answer = x*y
+
+print(answer)
+
+hight = float(input ("Ange din längd i meter"))
+weight = float(input ("Ange din vikt i kg"))
+ 
+
+hight2 = (hight**2)
+BMI = (weight / hight2) 
+print("din BMI är",BMI)
+ 
