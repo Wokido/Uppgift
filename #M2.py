ab43a48d21d35bd5cf892a92f7e327a82d03e737
Wokido <nilslukasmalcon.garpo@elev.ga.dbgy.se>
2025-09-03 13:18:21 +0200

    Add files via upload

---
+++ #M2.py
@@ -0,0 +1,49 @@
+#M2
+print("10+67=",10+67)#plus
+print("67-14=",67-14)#minus
+print("5*8=",5*8)#multiply
+print("56/7=",56/7)#divide
+print("10//3=",10//3)#floor division Dividerar och rundar ner till närmaste heltal.
+print("10%3=",10%3)#modulus Tar resten vid heltalsdivision
+print("2**3=",2**3)#exponentiation Upphöjt till
+
+#uppgift 2
+
+print("Hej! Välkommen till mitt program.") 
+namn =input("vad heter du?")
+print(f"Trevligt att träffas, {namn}! Jag heter Lukas.")
+
+age =input("how old are you")
+print(f"WOW du är {age} ig")
+
+#Uppgift 3
+
+x = float(input ("Ange första talet"))
+y = float(input ("Ange andra talet"))
+
+answer = x*y
+
+print(answer)
+
+# Uppgift 4
+
+hight = float(input ("Ange din längd i meter"))
+weight = float(input ("Ange din vikt i kg"))
+ 
+
+hight2 = (hight**2)
+BMI = (weight / hight2) 
+print("din BMI är",BMI)
+
+# Uppgift 5
+
+age_years = int(input("Ange din ålder i år: "))
+weeks_lived = age_years * 52
+print("Du har levt i ungefär", weeks_lived, "veckor.")
+
+
+# Uppgift 6
+
+kg = float(input("Ange din vikt i kilogram: "))
+lbs = kg * 2.20462
+print("Din vikt i pounds (lbs) är:", (lbs, 2))
